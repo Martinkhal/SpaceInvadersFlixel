e89517d3dc44826d1547637d68b9dff146d73827
Martinkhal <livemartin90@gmail.com>
2016-09-09 15:30:47 -0300

    Agregue Enemigo
    Aparece en pantalla

---
+++ SpaceInvadersProject/source/clases/Enemigo.hx
@@ -0,0 +1,33 @@
+package sprites;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.FlxG;
+import sprites.Enemigo;
+import sprites.Balas;
+/**
+ * ...
+ * @author ...
+ */
+class Enemigo extends FlxSprite
+{
+
+	
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		
+		//PARA CREAR ENEMIGO/imagen
+		loadGraphic(AssetPaths.enemigo1__png); //para cargar la direccion de una imagen y usarla
+		//makeGraphic(64, 64, 0xFFFFFF00);
+		setGraphicSize(30, 30); //tamaño de la imagen
+		
+	}
+	
+	override public function update (elapsed:Float):Void
+	{
+		super.update(elapsed);
+		
+
+	}
+}
