9764611bc55ddb09efe674900f4309b2ab008be1
Martinkhal <livemartin90@gmail.com>
2016-09-09 15:27:44 -0300

    La Navecita se mueve lateralmente y choca contra los bordes de la pantalla

--- SpaceInvadersProject/source/clases/Navecita.hx
+++ SpaceInvadersProject/source/clases/Navecita.hx
@@ -1,15 +1,69 @@
-package source.clases;
+package sprites;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+import flixel.FlxG;
 
 /**
  * ...
  * @author ...
  */
-class Navecita
+class Player extends FlxSprite
 {
 
-	public function new() 
+	
+	public function new(?X:Float=0, ?Y:Float=0) 
 	{
+		super(X, Y);
+		
+		loadGraphic(AssetPaths.navejugador__png);
+		setGraphicSize(45, 45);
 		
 	}
 	
+	override public function update (elapsed:Float):Void
+	{
+		super.update(elapsed);
+		
+		Movimiento();
+		
+		
+			
+		if (FlxG.keys.justPressed.J)
+		{
+			Disparar();
+		}
+	}
+	public function Movimiento()
+	{
+		if (FlxG.keys.pressed.D)
+		x += 3;
+		
+		if (FlxG.keys.pressed.A)
+		x -= 3;
+		
+		RestringirMovimiento();
+		
+	}
+	//PARA QUE PLAYER NO SE MUEVA MAS ALLA DE LA PANTALLA
+	public function RestringirMovimiento()
+	{
+		
+		
+		if(x > FlxG.width - width)	 
+			x = FlxG.width - width; 
+			
+		if(x < 0)
+			x = 0;
+	}
+	public function Disparar()
+	{
+			var b:Balas = new Balas();//aca es donde se diria que el objeto es INSTANCIADO
+			b.x = x + width / 2;
+			b.y = y + height / 16;
+			
+			FlxG.state.add(b);
+			
+			//FlxG.state.add(new Balas(x + width, y + heigth / 2));
+	}
 }
